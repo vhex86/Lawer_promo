066f90f3d5cc20a8aeae1368746c94ef0fd17fdf
vhex86 <vhex86@gmail.com>
2023-07-01 17:18:48 +0500

    Continue work for animate

--- scripts/animate_blog_section.js
+++ scripts/animate_blog_section.js
@@ -1,10 +1,37 @@
+let divElement = document.getElementById('Blog');
+let imgElement = document.createElement('img');
+
+imgElement.src = './img/like.svg';
+
+function placeImageRandomly() {
+    // Получение размеров и положения div
+    var divWidth = divElement.offsetWidth;
+    var divHeight = divElement.offsetHeight;
+    var divLeft = divElement.offsetLeft;
+    var divTop = divElement.offsetTop;
+  
+    // Вычисление случайных координат для изображения
+    var randomLeft = Math.random() * divWidth;
+    var randomTop = Math.random() * divHeight;
+  
+    // Установка позиции изображения
+    imgElement.style.position = 'absolute';
+    imgElement.style.left = divLeft + randomLeft + 'px';
+    imgElement.style.top = divTop + randomTop + 'px';
+  
+    // Добавление изображения в div
+    divElement.appendChild(imgElement);
+  }
+  
+
+
 let observer = new IntersectionObserver(function(entries) {
     entries.forEach(function(entry) {
       if (entry.isIntersecting && entry.intersectionRatio === 1) {
         // Если элемент полностью попал в область видимости
-        // alert('Common!');
+        alert('Common!');
        
-
+        placeImageRandomly();
 
 
 
