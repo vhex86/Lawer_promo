945368b0e745d43413bd621d6b4656ed4f60353e
vhex86 <vhex86@gmail.com>
2023-07-05 16:17:04 +0500

    Reverse change for animate reason - bugs

--- scripts/animate_blog_section.js
+++ scripts/animate_blog_section.js
@@ -1,15 +1,79 @@
 
 
+// let MatrixWithImgSrc = [ "./img/camera.svg", " ./img/like.svg" , "./img/video.svg", "./img/man.svg", "./img/bubble.svg" ];
+
+// let interval;
+// let imgElement; // Добавляем переменную для хранения текущего imgElement
+// function placeImageRandomly(){
+
+//  interval = setInterval(() => {
+   
+
+//     imgElement = document.createElement('img');
+//     let randomElement = Math.floor(Math.random() * 5);
+   
+//     imgElement.src = MatrixWithImgSrc[randomElement];
+    
+//     let divWidth = Blog.offsetWidth;
+//     let divHeight = Blog.offsetHeight;
+
+//     let randomLeft = divWidth - (Math.random() * divWidth);
+//     let randomTop = divHeight - (Math.random() * divHeight);
+
+//     imgElement.style.position = 'absolute';
+//     imgElement.style.top = randomTop + 'px';
+//     imgElement.style.left = randomLeft + 'px';
+
+//     Blog.appendChild(imgElement);
+  
+//     setTimeout(() => {
+//        if (imgElement.parentNode === Blog) {
+//       Blog.removeChild(imgElement);} // Удаление изображения из контейнера
+//     }, 2000); // Задержка перед удалением
+
+//   }, 500); 
+
+// }
+// function removeImagesInBlogSection() {
+//   let imgElements = document.querySelectorAll('#Blog img');
+//   imgElements.forEach(function(imgElement) {
+//     imgElement.remove();
+//   });
+// }
+
+
+
+
+// let observer = new IntersectionObserver(function(entries) {
+//   entries.forEach(function(entry) {
+//     if (entry.isIntersecting && entry.intersectionRatio === 1 )  {
+//       placeImageRandomly();
+
+//     }
+// else {
+
+//   removeImagesInBlogSection();
+//   clearInterval(interval); 
+
+// }
+
+
+//   });
+// }, { threshold: 1 });
+
+// let targetElement = document.querySelector('.button_blog');
+// observer.observe(targetElement);
+
+
 let MatrixWithImgSrc = [ "./img/camera.svg", " ./img/like.svg" , "./img/video.svg", "./img/man.svg", "./img/bubble.svg" ];
 
 let interval;
-let imgElement; // Добавляем переменную для хранения текущего imgElement
-function placeImageRandomly(){
+function placeImageRandomly() {
 
  interval = setInterval(() => {
    
 
-    imgElement = document.createElement('img');
+    let imgElement = document.createElement('img');
     let randomElement = Math.floor(Math.random() * 5);
    
     imgElement.src = MatrixWithImgSrc[randomElement];
@@ -27,8 +91,7 @@ function placeImageRandomly(){
     Blog.appendChild(imgElement);
   
     setTimeout(() => {
-       if (imgElement.parentNode === Blog) {
-      Blog.removeChild(imgElement);} // Удаление изображения из контейнера
+      Blog.removeChild(imgElement); // Удаление изображения из контейнера
     }, 2000); // Задержка перед удалением
 
   }, 500); 
@@ -63,3 +126,4 @@ else {
 
 let targetElement = document.querySelector('.button_blog');
 observer.observe(targetElement);
+
